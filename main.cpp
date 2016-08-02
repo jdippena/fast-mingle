37ce2454a744b358deb8c7530c65f68244ff4032
jdippena <jdippena@macalester.edu>
2016-08-02 17:21:11 -0500

    Simple hello world OpenGL/GLUT window.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
 #include "main.h"
-#include "ANN/ANN.h"
 
 using namespace std;
 
-int main() {
+void RenderScene(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLineWidth(20.0f);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(15, 0, 0);
+    glEnd();
+    glFlush();
+}
+
+void SetupRC(void)
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+
+int main(int argc, char *argv[]) {
     int	nPts;
     ANNpointArray dataPts;
     ANNpoint queryPt;
@@ -19,6 +35,13 @@ int main() {
     dists = new ANNdist[10];
 
     kdTree = new ANNkd_tree(dataPts, nPts, 4);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Simple");
+    glutDisplayFunc(RenderScene);
+    SetupRC();
+    glutMainLoop();
 
     return 0;
 }
--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MINGLEC_MAIN_H
 #define MINGLEC_MAIN_H
 
+#include <iostream>
 #include <math.h>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#include "ANN/ANN.h"
+#include "libpng-1.6.23/include/png.h"
 
 #endif //MINGLEC_MAIN_H
 
