15643a8f182b1a499e9872c3fb14c75760a771f7
Shilad Sen <Shilad.Sen@target.com>
2016-08-26 22:02:15 -0500

    Write postgres friendly output file.

--- EdgeBundler.h
+++ EdgeBundler.h
@@ -20,6 +20,7 @@ public:
     void setDrawBezierFunction(void (*drawBezierFunction)(const Point *start, const Point *ctrl1, const Point *ctrl2, const Point *end, const int weight));
     void renderLines();
     void renderBezier();
+    void write(char *path);
 
 private:
     EdgeBundleTree::Edge *edges;
@@ -35,6 +36,11 @@ private:
     void makeTopEdgesMap(std::unordered_map<int, EdgeBundleTree::Edge*> *map);
     void drawEdgeLines(EdgeBundleTree::Edge *edge);
     void drawEdgeBeziers(const EdgeBundleTree::Edge *edge, const Point *sPointTo, const Point *tPointTo);
+
+    void writeBundle(FILE *out, EdgeBundleTree::Edge *tree);
+    void writeEdgeBeziers(FILE *out, std::vector<const EdgeBundleTree::Edge *> *segments, const EdgeBundleTree::Edge *edge, const Point *sPointTo, const Point *tPointTo);
+    void writeOneEdgeBezier(FILE *out, const Point *start, Point *startCurve, const Point *ctrl1, const Point *ctrl2, const Point *end);
+
 };
 
 #endif //MINGLEC_EDGEBUNDLER_H
